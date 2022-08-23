8b8cc11fa6c460c003927e2bf37ed583c117cd93
Bruno Paz <brunopaz00@yahoo.com.mx>
2022-08-23 16:00:28 -0500

    primer commit

--- src/Screens/MainPage.js
+++ src/Screens/MainPage.js
@@ -1,9 +1,13 @@
 import React from "react"
-
+import './MainPage.css'
 const MainPage = () => {
     return (
-        <div>
-
+        <div className="body">
+            <div className="main-container">
+                <div className="top-container"></div>
+                <div className="bottom-container"></div>
+            </div>
+            <footer></footer>
         </div>
     )
 }
