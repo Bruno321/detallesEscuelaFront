7178270d29c5d06bc7cdf09fc8f2f51064240287
Alda <caldair99@gmail.com>
2022-08-23 16:50:04 -0500

    Search input

---
+++ src/Components/Searcher/Searcher.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Searcher.css"
+
+const SearchInput = () => {
+    return (
+        <div className="main-container">
+            <input type="text" placeholder="Ingresa el nombre de un maestro o salón" className="SearchInput"/>
+        </div>
+    )
+}
+
+export default SearchInput
